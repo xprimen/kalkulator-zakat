b669b37001d78356f664b9c6bea9dfc76178fd97
xprimen <engga.muse@gmail.com>
2024-03-01 14:56:48 +0700

    Revert "Menambahkan speed insight vercel"
    This reverts commit 6cc0dd21cd2686e4bd5226058095f07dc67fce53.

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,14 @@
-import "../styles/globals.css";
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import '../styles/globals.css';
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
       <Analytics />
-      <SpeedInsights />
     </>
   );
 }
 
 export default MyApp;
+
