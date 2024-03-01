6cc0dd21cd2686e4bd5226058095f07dc67fce53
xprimen <engga.muse@gmail.com>
2024-03-01 09:25:38 +0700

    Menambahkan speed insight vercel

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,15 @@
-import '../styles/globals.css';
-import { Analytics } from '@vercel/analytics/react';
+import "../styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights />
     </>
   );
 }
 
 export default MyApp;
-
